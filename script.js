f7df6a15be59bc94ed542133272f2f9b76d93572
Sharon Babu <sharonbabu107@gmail.com>
2023-07-06 20:29:48 +0530

    portfolio finished

---
+++ script.js
@@ -0,0 +1,7 @@
+function resume() {
+	window.location="https://github.com/sharonbabusharon/resume/edit/main/index.html"
+}
+
+function reach(){
+	window.location="https://www.linkedin.com/in/sharon-babu-9669b3245/"
+}
